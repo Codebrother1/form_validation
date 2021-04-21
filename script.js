d50ed173bbe3db83a858ee3c1864b880e1dcc7a6
Terrance Cazy <terrancecazy@MacBook-Pro.lan>
2021-04-21 16:33:54 -0500

    email validation and emails match, complete form

--- script.js
+++ script.js
@@ -16,9 +16,13 @@ function showSuccess(input){
   formControl.className = 'form-control success';
 }
 
-function isValidEmail(email){
+function checkEmail(input){
   const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
+    if(re.test(input.value.trim())){
+      showSuccess(input)
+    }else{
+      showError(input, 'Email is not valid');
+    }
 }
 
 function checkRequired(inputArr){
@@ -31,6 +35,26 @@ function checkRequired(inputArr){
     }
   });
 }
+
+function checkLength(input, min, max){
+  if(input.value.length < min){
+    showError(input, `${getFieldName(input)} must be at least ${min} characters`)
+  } else if(input.value.lenght > max){
+    showError(input, `${getFieldName(input)} must less than ${max} characters`)
+  } else{
+    showSuccess(input)
+  }
+}
+
+function checkPasswordsMatch(input1, input2){
+  if(input1.value !== input2.value){
+    showError(input2, "Passwords do not match")
+  }
+}
+
+
+
+
 function getFieldName(input){
   return input.id.charAt(0).toUpperCase() + input.id.slice(1);
 }
@@ -39,6 +63,9 @@ function getFieldName(input){
 form.addEventListener('submit', function(e){
   e.preventDefault();
   checkRequired([username, email, password, password2])
-
+  checkLength(username, 3, 15);
+  checkLength(password, 6, 20);
+  checkEmail(email);
+  checkPasswordsMatch(password, password2)
 });
 
