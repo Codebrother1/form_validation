5fd1655c7926437d0bf191165f5daa2f53f94653
Terrance Cazy <terrancecazy@MacBook-Pro.lan>
2021-04-21 16:18:04 -0500

    check required inputs

---
+++ script.js
@@ -0,0 +1,44 @@
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
+
+function showError(input, message){
+  const formControl = input.parentElement;
+  formControl.className = 'form-control error';
+  const small = formControl.querySelector('small')
+  small.innerText = message;
+}
+
+function showSuccess(input){
+  const formControl = input.parentElement;
+  formControl.className = 'form-control success';
+}
+
+function isValidEmail(email){
+  const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+function checkRequired(inputArr){
+  inputArr.forEach(function(input){
+    if(input.value.trim() === ""){
+      
+      showError(input, `${getFieldName(input)} is required`);
+    }else{
+      showSuccess(input)
+    }
+  });
+}
+function getFieldName(input){
+  return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+}
+
+
+form.addEventListener('submit', function(e){
+  e.preventDefault();
+  checkRequired([username, email, password, password2])
+
+});
+
